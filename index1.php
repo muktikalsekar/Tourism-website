39960a8d02b0901cb3258c8c890165e22d6394fe
mukti <46982965+muktikalsekar@users.noreply.github.com>
2020-07-13 22:52:07 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,253 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<style>
+body{
+	margin:0;
+	background:url('bg.jpg');
+	background-size:cover;
+}
+.nav{
+	width:100%;
+	background:#000033;
+	height:100px;
+	margin-top:0%;
+}
+ul{
+	list-style:none;
+	padding:0;
+	margin:0;
+	position:top;
+}
+li{
+	float:left;
+	margin-top:30px;
+}
+a{
+	width:150px;
+	color:white;
+	display:block;
+	text-decoration:none;
+	font-size:20px;
+	text-align:center;
+	padding:10px;
+	border-radius:10px;
+	font-family:century gothic;
+	font-weight:bold;
+}
+a:hover{
+	background:#660000;
+	trasition:0.6s;
+}
+</style>
+</head>
+
+<body background="11.jpg">
+<marquee bgcolor="orange" loop="-4" scrollamount="7" width="100%" height="7%"><font face="copper black" color='white' size="6%"><b><i>कभी तोह आओ गुजरात मैं....|કભી ટૂ આઓ ગુજરાત મુખ્ય....</i></b> </marquee>
+<table align="center" cellpadding=20px>
+
+<tr>
+<td><img src= "1.jpg" width="90%" height="80px" alt="image" /> </td>
+<td> </td>
+<td><h1 align='centre'><font size="10%" color='white' font face="bernand mt condensed">
+<i><b>Gujarat Tourism</b></i></font></h1>
+<hr width="100%" color="orange" size="4" /></td>
+</table>
+
+<table cellpadding="20">
+<tr>
+<td><a href="index1.php">HOME</a></td>
+<td><a href="destination.html">DESTINATION</a></td>
+<td><a href="hotel.html">HOTEL</a></td>
+<td><a href="about.html">ABOUT US</a></td>
+<td><a href="feed.php">FEEDBACK</a></td>
+
+
+</table>
+
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
+</head>
+<body>
+
+
+
+<div class="slideshow-container" style="width:100%" "height:+---">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 9</div>
+  <img src="sabarmati.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><h1>SABARMATI ASHRAM</h1></div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 9</div>
+  <img src="8.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><h1>GANDHINAGAR</h1></div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 9</div>
+  <img src="2.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><font  color="white"><h1>AMAAZIA WATER PARK </h1></font></div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">4 / 9</div>
+  <img src="9.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><h1>SURAT</h1> </div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">5 / 9</div>
+  <img src="5.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><h1>BARODA MUSEUMS AND PHOTO GALLERY</h1> </div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">6 / 9</div>
+  <img src="maqbara.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><h1>MAHABAT MAQBARA</h1> </div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">7 / 9</div>
+  <img src="modhera tem.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><h1>MODHERA TEMPLE </h1></div>
+</div>
+
+<div class="mySlides fade" >
+  <div class="numbertext">8 / 9</div>
+  <img src="sidi sayeed.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><h1>SIDI SAYEED'S MOSQUE</h1> </div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">9 / 9</div>
+  <img src="10.jpg" class="w3-border" alt="Norway" style="padding:10px;width:100%;height:500px">
+  <div class="text"><h1>INDRODA NATURE PARK</h1> </div>
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center" color: "white" font size="100%">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+<span class="dot"></span> 
+<span class="dot"></span> 
+<span class="dot"></span>
+  </div>
+
+
+ <script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
+
+
+
+
+</body>
+
+
+
+</body>
+
+</html>
+</html>
